646a13287d5581347272fb3d18eb7a134d9703d8
Vinícius Longo <longo.vinicius@gmail.com>
2025-07-22 15:46:38 -0300

    feat: simplification of state machine and inclusion of busy_o signal

--- linear-solver/src/LinearSolver_Manager.vhd
+++ linear-solver/src/LinearSolver_Manager.vhd
@@ -1,3 +1,25 @@
+--! \file		LinearSolver_Manager.vhd
+--!
+--! \brief		
+--!
+--! \author		Vinícius de Carvalho Monteiro Longo (longo@weg.net)
+--! \date       22-07-2025
+--!
+--! \version    1.0
+--!
+--! \copyright	Copyright (c) 2024 WEG - All Rights reserved.
+--!
+--! \note		Target devices : No specific target
+--! \note		Tool versions  : No specific tool
+--! \note		Dependencies   : No specific dependencies
+--!
+--! \ingroup	None
+--! \warning	None
+--!
+--! \note		Revisions:
+--!				- 1.0	22-07-2025	<longo@weg.net>
+--!				First revision.
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -13,68 +35,62 @@ entity LinearSolver_Manager is
         sysclk              : in std_logic;
         reset_n             : in std_logic;
         init_calc_i         : in std_logic;
-        Amatrix_i             : in matrix_fp_t(0 to N_SS - 1, 0 to N_SS - 1);
-        Bmatrix_i             : in matrix_fp_t(0 to N_SS - 1, 0 to N_IN - 1); 
+        Amatrix_i           : in matrix_fp_t(0 to N_SS - 1, 0 to N_SS - 1);
+        Bmatrix_i           : in matrix_fp_t(0 to N_SS - 1, 0 to N_IN - 1); 
         Xvec_initial        : in vector_fp_t(0 to N_SS - 1); 
         Uvector_i           : in vector_fp_t(0 to N_IN - 1);
-        Xvec_current_o      : out vector_fp_t(0 to N_SS - 1)
+        Xvec_current_o      : out vector_fp_t(0 to N_SS - 1);
+        busy_o              : out std_logic
 
     );
 end entity;
 
 architecture arch of LinearSolver_Manager is
-
-    signal start_i_sig        : std_logic;
-    signal busy_sig           : std_logic;
-    signal X_next_sig         : vector_fp_t(0 to N_SS - 1);
-    signal X_current_sig      : vector_fp_t(0 to N_SS - 1) := Xvec_initial;
-
+    
     type state_t is (S_IDLE, S_BUSY);
-    signal current_state    : state_t := S_IDLE;
+    signal current_state        : state_t := S_IDLE;
+    signal busy_sig             : std_logic;
+    signal X_next_sig           : vector_fp_t(0 to N_SS - 1);
+    signal X_current_sig        : vector_fp_t(0 to N_SS - 1);
 
 begin
 
     LSH: entity work.LinearSolver_Handler
     generic map(
-        N_SS => N_SS,
-        N_IN => N_IN
+        N_SS                => N_SS,
+        N_IN                => N_IN
     )
     port map(
-        sysclk      => sysclk,
-        start_i     => start_i_sig,
-        Amatrix_i   => Amatrix_i,
-        Xvec_i      => X_current_sig, 
-        Bmatrix_i   => Bmatrix_i,
-        Uvec_i      => Uvector_i,
-        Xvec_next_o => X_next_sig,   
-        busy_o      => busy_sig
+        sysclk              => sysclk,
+        start_i             => init_calc_i,
+        Amatrix_i           => Amatrix_i,
+        Xvec_i              => X_current_sig, 
+        Bmatrix_i           => Bmatrix_i,
+        Uvec_i              => Uvector_i,
+        stateResultVec_o    => X_next_sig,   
+        busy_o              => busy_sig
     );
 
-    State_Machine_Process: process(sysclk)
-    begin
-        if reset_n = '0' then
-            current_state <= S_IDLE;
+    State_Machine_Process: process(reset_n, sysclk)
+    begin 
+        if reset_n = '0' then 
             X_current_sig <= Xvec_initial;
-            start_i_sig <= '0';
         elsif rising_edge(sysclk) then
             case current_state is
                 when S_IDLE =>
-                    start_i_sig <= '0';
-                    if init_calc_i = '1' and busy_sig = '0' then
-                        start_i_sig <= '1'; 
+                    if init_calc_i = '1' then 
                         current_state <= S_BUSY;
-                    end if;                   
+                    end if;
                 when S_BUSY =>
-                    start_i_sig <= '0';
-                    if busy_sig = '0' then
-                        X_current_sig <= X_next_sig; 
-                        current_state <= S_IDLE; 
-                    end if;    
+                    if busy_sig = '0' then 
+                        X_current_sig <= X_next_sig;
+                        current_state <= S_IDLE;
+                    end if;
             end case;
         end if;
-    end process State_Machine_Process;
-
-    Xvec_current_o <= X_current_sig;
+    end process; 
     
+    Xvec_current_o <=  X_current_sig;           
+    busy_o         <=  busy_sig;
 
-end arch;
+end arch ; -- arch
